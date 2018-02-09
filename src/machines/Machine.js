ba8bdfb6d21bfb95bcd7d56403f010eea72377c8
Christopher Miller <admin@mctherealm.net>
2018-02-08 20:00:33 -0500

    Removed durability

--- src/machines/Machine.js
+++ src/machines/Machine.js
@@ -6,9 +6,6 @@ export default class Machine {
     this.bps = bonesPerSecond;
     this.upm = unitPriceMultiplier;
     this.count = 0;
-    this.totalLifetime = perLife;
-    this.startingLifeTime = this.totalLifetime;
-    this.currentLifetime = 0;
 
     this.unitCost = function() {
       return Math.ceil(this.price * Math.pow(1 + this.upm, this.count));
@@ -17,22 +14,9 @@ export default class Machine {
     this.totalBPS = function() {
       return this.bps * this.count;
     }
-
-    this.tickLifetime = function() {
-      if (this.count > 0) {
-        this.currentLifetime -= 0.1;
-        if (this.currentLifetime <= 0) {
-          this.count--;
-          if (this.count !== 0) {
-            this.currentLifetime = this.totalLifetime;
-          }
-        }
-      }
-    }
   }
 
   purchaseMachine() {
     this.count++;
-    this.currentLifetime = this.totalLifetime;
   }
 }
